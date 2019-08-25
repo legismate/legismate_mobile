cffbd4294aae5e83d2b26bafd0cf8b084c58dc3a
Scott Smith <mail@ScottHSmith.com>
2019-08-25 15:39:58 -0500

    Add Registration dialog.

--- lib/screens/districts.dart
+++ lib/screens/districts.dart
@@ -1,5 +1,49 @@
 import 'package:flutter/material.dart';
 
+class DatesCard extends StatelessWidget {
+  DatesCard({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+        padding: EdgeInsets.only(top: 20, left: 20, right: 20),
+        margin: EdgeInsets.all(0),
+        alignment: Alignment.centerLeft,
+        child: Center(
+          child: Card(
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: <Widget>[
+                const ListTile(
+                  title: Text('30 Aug. Voting registration closes'),
+                  subtitle: Text('10 Sept. Voting closes'),
+                ),
+                ButtonTheme.bar(
+                  // make buttons use the appropriate styles for cards
+                  child: ButtonBar(
+                    children: <Widget>[
+                      FlatButton(
+                        child: const Text('Register'),
+                        onPressed: () {
+                          /* ... */
+                        },
+                      ),
+                      FlatButton(
+                        child: const Text('Dismiss'),
+                        onPressed: () {
+                          /* ... */
+                        },
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ));
+  }
+}
+
 class Districts extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
@@ -29,66 +73,83 @@ class Districts extends StatelessWidget {
     );
 
     Widget personCard = Container(
-      padding: const EdgeInsets.all(8),
-      alignment: Alignment.centerLeft,
-      color: Colors.black12,
-      child: Column(
-        children: [
-//          Image.asset(
+        alignment: Alignment.centerLeft,
+//        alignment: Alignment(0, 0),
+        color: Colors.black12,
+//      child: Column(
+//        children: [
+//          Image.network(
 //            'https://picsum.photos/200',
 //            fit: BoxFit.cover,
 //          ),
+//          Text(
+//            'Aaron Andrews',
+//            softWrap: true,
+//            style: Theme.of(context).textTheme.title,
+//          ),
+//        ],
+//      ),
+        child: Column(children: <Widget>[
+          Expanded(
+            child: FittedBox(
+              fit: BoxFit.cover,
+              child: Image.network(
+                'https://picsum.photos/200',
+                fit: BoxFit.cover,
+              ),
+            ),
+          ),
           Text(
             'Aaron Andrews',
             softWrap: true,
             style: Theme.of(context).textTheme.title,
           ),
-        ],
-      ),
-    );
+        ]));
 
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Legismate'),
-      ),
-      bottomNavigationBar: BottomNavigationBar(
+        appBar: AppBar(
+          title: Text('Legismate'),
+        ),
+        bottomNavigationBar: BottomNavigationBar(
 //          onTap: (newIndex) => setState(() => _index = newIndex),
 //          currentIndex: _index,
-        selectedItemColor: Colors.black,
-        items: [
-          BottomNavigationBarItem(
-            icon: new Icon(Icons.people),
-            title: new Text("People"),
+          selectedItemColor: Colors.black,
+          items: [
+            BottomNavigationBarItem(
+              icon: new Icon(Icons.people),
+              title: new Text("People"),
+            ),
+            BottomNavigationBarItem(
+              icon: new Icon(Icons.view_list),
+              title: new Text("Bills"),
+            ),
+          ],
+        ),
+        body: Container(
+            child: CustomScrollView(slivers: <Widget>[
+          SliverList(
+            delegate: SliverChildListDelegate(
+              [DatesCard()],
+            ),
           ),
-          BottomNavigationBarItem(
-            icon: new Icon(Icons.view_list),
-            title: new Text("Bills"),
+          SliverList(
+            delegate: SliverChildListDelegate(
+              [districtTitleSection],
+            ),
           ),
-        ],
-      ),
-      body: Container(
-          child: CustomScrollView(slivers: <Widget>[
-            SliverList(
+          SliverPadding(
+            padding: const EdgeInsets.symmetric(horizontal: 20),
+            sliver: SliverGrid(
+              gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                  crossAxisCount: 2,
+//                  childAspectRatio: 1.0,
+                  crossAxisSpacing: 20,
+                  mainAxisSpacing: 20),
               delegate: SliverChildListDelegate(
-                [districtTitleSection],
+                [personCard, personCard, personCard],
               ),
             ),
-            new SliverPadding(
-              padding: const EdgeInsets.symmetric(horizontal: 20),
-              sliver: SliverGrid(
-                gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                    crossAxisCount: 2,
-                    childAspectRatio: 1.0,
-                    crossAxisSpacing: 20,
-                    mainAxisSpacing: 20),
-                delegate: SliverChildListDelegate(
-                  [personCard, personCard, personCard],
-                ),
-              ),
-            ),
-          ]))
-    );
-
-
+          ),
+        ])));
   }
 }
